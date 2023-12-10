48340c196adcdba144cf5fe025e6a7e17f08761a
alena <misamistake@>
2023-12-10 19:08:37 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-
         int a = 20000;
         byte b = 126;
         short c = 2000;
